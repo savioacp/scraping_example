f4bb11dd0921a09ecec444747873f2ff3d77b2c1
savioacp <savioacp@gmail.com>
2021-02-02 17:30:56 -0300

    Task rename
    Renamed scrape task file to "scrape.ex" instead of "run.ex"

--- lib/mix/tasks/scrape.ex
+++ lib/mix/tasks/scrape.ex
@@ -3,12 +3,11 @@ defmodule Mix.Tasks.Scrape do
 
   @impl Mix.Task
   def run(args) do
-    page = hd(if(args === [], do: [1], else: args))
 
     Mix.Tasks.Compile.run([])
 
     Mix.Tasks.App.Start.run([])
 
-    ScrapingExample.main(page)
+    ScrapingExample.main(args)
   end
 end
